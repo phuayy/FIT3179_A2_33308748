0f2ca67184bb55eeb506ac7b349f1f49e0cc5196
Hairy Potter <HideInBetweenLeg@users.noreply.github.com>
2025-10-21 01:37:37 +0800

    radar chart added

--- script.js
+++ script.js
@@ -1,2 +1,6 @@
 var vg_evmap = "./js/player_origin.json";
 vegaEmbed("#Choropleth_map", vg_evmap, {actions: false}).then(function(res){}).catch(console.error);
+
+var vg_radarChart = "./js/nrg_player_radar_v2.json";
+
+vegaEmbed("#Radar_chart", vg_radarChart, {actions: false}).then(function(res){}).catch(console.error);
