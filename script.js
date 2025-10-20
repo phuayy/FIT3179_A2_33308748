b90a4edb6e2e13ae683f92a9749e571f17bd35ed
Hairy Potter <HideInBetweenLeg@users.noreply.github.com>
2025-10-21 02:11:52 +0800

    all nrg players radar added

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 var vg_evmap = "./js/player_origin.json";
 vegaEmbed("#Choropleth_map", vg_evmap, {actions: false}).then(function(res){}).catch(console.error);
 
-var vg_radarChart = "./js/s0m_player_radar_v2.json";
+var vg_radarChart = "./js/s0m_player_radar.json";
 
-vegaEmbed("#Radar_chart", vg_radarChart, {actions: false}).then(function(res){}).catch(console.error);
+// 2. Embed the chart in the div with the ID "Radar_chart"
+vegaEmbed("#Radar_chart", vg_radarChart, {actions: false})
+  .then(function(res){
+    console.log("Radar chart loaded successfully");
+  })
+  .catch(console.error);
