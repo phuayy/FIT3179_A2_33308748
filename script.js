abfd475fe929dd5593ab478384ed995743eac310
Hairy Potter <HideInBetweenLeg@users.noreply.github.com>
2025-10-21 02:05:44 +0800

    s0m added

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 var vg_evmap = "./js/player_origin.json";
 vegaEmbed("#Choropleth_map", vg_evmap, {actions: false}).then(function(res){}).catch(console.error);
 
-var vg_radarChart = "./js/nrg_player_radar_v2.json";
+var vg_radarChart = "./js/s0m_player_radar_v2.json";
 
 vegaEmbed("#Radar_chart", vg_radarChart, {actions: false}).then(function(res){}).catch(console.error);
